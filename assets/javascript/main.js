32f3a8e6b934022880ef4f57ec1c71f217de2500
sahil narang <32524427+sahilnar@users.noreply.github.com>
2022-07-20 09:48:40 -0700

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,6 +4,8 @@ var button2;
 var content1;
 var content2;
 function initEvent(){
+    var x=false;
+    var y=false;
     button1 = document.querySelector("#btn1");
     button2 = document.querySelector("#btn2");
     content1 = document.querySelector(".service-1");
@@ -24,4 +26,39 @@ function initEvent(){
         button1.style.backgroundColor = "#007bff";
         button1.style.color = "#fff";
     });
+}
+function checkName(n){
+    if(n.value == "")
+    {
+        n.style.border = "2px solid red";
+        n.style.boxShadow = "1px 1px 2px red";
+    }
+    else{
+        n.style.border = "2px solid green";
+        n.style.boxShadow = "1px 1px 2px green";
+        x = true;
+    }
+    return x;
+}
+function checkEmail(n){
+    if(!document.querySelector(".email").validity.valid)
+    {
+        n.style.border = "2px solid red";
+        n.style.boxShadow = "1px 1px 2px red";
+        document.querySelector(".x").innerHTML = "Please Enter Valid Email Id";
+        document.querySelector(".x").style.color = "red";
+        document.querySelector(".x").style.fontSize = "100%";
+    }
+    else{
+        n.style.border = "2px solid green";
+        n.style.boxShadow = "1px 1px 2px green";
+        document.querySelector(".x").innerHTML = "";
+        y = true;
+    }
+    return y;
+}
+function showMessage(n){
+    if(x==true && y==true){
+        alert("Your response has been Sent to Sahil Narang");
+    }
 }
