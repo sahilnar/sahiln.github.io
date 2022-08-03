b6d76c5a9e78604c5b3ae544dbd23c2bb88d69c2
sahil narang <32524427+sahilnar@users.noreply.github.com>
2022-08-02 17:07:39 -0700

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,8 +4,6 @@ var button2;
 var content1;
 var content2;
 function initEvent(){
-    var x=false;
-    var y=false;
     button1 = document.querySelector("#btn1");
     button2 = document.querySelector("#btn2");
     content1 = document.querySelector(".service-1");
@@ -26,57 +24,5 @@ function initEvent(){
         button1.style.backgroundColor = "#007bff";
         button1.style.color = "#fff";
     });
-}
-function validateName(n){
-    if(n.value == "")
-    {
-        n.style.border = "2px solid rgb(255, 150, 150)";
-    }
-    else{
-        n.style.border = "2px solid green";
-        x = true;
-    }
-    return x;
-}
-function validateNum(n){
-    if(!n.validity.valid)
-    {
-        n.style.border = "2px solid rgb(255, 150, 150)";
-        document.querySelector(".y").innerHTML = "Please Enter Valid Phone Number";
-        document.querySelector(".y").style.color = "red";
-        document.querySelector(".y").style.fontSize = "100%";
-    }
-    else{
-        n.style.border = "2px solid green";
-        document.querySelector(".y").innerHTML = "";
-        y = true;
-    }
-    return x;
-}
-function checkEmail(n){
-    if(!n.validity.valid)
-    {
-        n.style.border = "2px solid rgb(255, 150, 150)";
-        document.querySelector(".x").innerHTML = "Please Enter Valid Email Id";
-        document.querySelector(".x").style.color = "red";
-        document.querySelector(".x").style.fontSize = "100%";
-    }
-    else{
-        n.style.border = "2px solid green";
-        document.querySelector(".x").innerHTML = "";
-        y = true;
-    }
-    return y;
-}
-function showMessage(n){
-    if(x==true && y==true){
-        alert("Your response has been Sent to Sahil Narang");
-    }
-}
-function changeopacity(){
-    var dateControl = document.querySelector('input[type="date"]');
-    if(dateControl.value != ""){
-        dateControl.style.opacity = "1";
-    }
 }
 
